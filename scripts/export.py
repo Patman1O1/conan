f1ee72cab1e5fcf05c23e47b8844cd0df9c948ad
Patman1O1 <patrickthomas101x@gmail.com>
2026-05-30 10:34:48 -0500

    Edit export.py

--- scripts/export.py
+++ scripts/export.py
@@ -3,40 +3,76 @@ from pathlib import Path
 import sys
 import shutil
 import click
+import traceback
 
-@click.command(name="export")
-@click.argument("pathname")
-@click.option("--recursive", "-r", is_flag=True, default=False)
+conan_home: Path = Path(ConanAPI().home_folder)
+conan_repo: Path = Path(__file__).resolve().parent.parent
+
+def _copy(src_path: Path, dst_path: Path, force: bool) -> None:
+    try:
+        # Ensure the source path leads to pre-existing file/directory
+        if not src_path.exists():
+            sys.stderr.write(f"No path named '{src_path}' exists\n")
+            sys.exit(1)
+
+        if dst_path.exists():
+            # Check if the force flag has been set true
+            if not force:
+                # Don't remove anything if the force flag is set to false
+                sys.stderr.write(f"The path '{dst_path}' already exists\n")
+                sys.exit(1)
+
+            # Remove the file or directory at the destination path
+            if dst_path.is_dir():
+                shutil.rmtree(dst_path)
+            else:
+                dst_path.unlink()
+
+        # Copy the new file/directory into the destination path
+        if src_path.is_dir():
+            shutil.copytree(src_path, dst_path, symlinks=True, dirs_exist_ok=True)
+        else:
+            shutil.copy2(src_path, dst_path)
+
+    except Exception as exception:
+        sys.stderr.write(f"{traceback.format_exception(exception)}\n")
+
+@click.group(name="export")
+def export() -> None:
+    return
+
+@export.command("template")
+@click.argument("name")
 @click.option("--force", "-f", is_flag=True, default=False)
-def export(pathname: str, recursive: bool, force: bool) -> None:
-    # Construct the path using the pathname string
-    path: Path = Path(pathname)
+def template(name: str, force: bool) -> None:
+    # Ensure <conan_home>/templates/command/new exists
+    template_dir: Path = Path("templates/command/new")
+    Path.mkdir(conan_home/template_dir, parents=True, exist_ok=True)
 
-    # Construct the source path
-    src_path: Path = Path(ConanAPI().home_folder)/path
+    _copy(conan_repo/template_dir/name, conan_home/template_dir/name, force)
+    return
 
-    # Ensure the source path exists
-    if not src_path.exists():
-        sys.stderr.write(f"No path named '{src_path}' exists\n")
-        sys.exit(1)
+@export.command("cmd")
+@click.argument("name")
+@click.option("--force", "-f", is_flag=True, default=False)
+def cmd(name: str, force: bool) -> None:
+    # Ensure <conan_home>/extensions/commands exists
+    cmd_dir: Path = Path("extensions/commands")
+    Path.mkdir(conan_home/cmd_dir, parents=True, exist_ok=True)
 
-    # Construct the destination path
-    dst_path: Path = Path(__file__).resolve().parent.parent/path
+    _copy(conan_repo/cmd_dir/name, conan_home/cmd_dir/name, force)
+    return
 
-    # Check if the destination path already exists and
-    # override it if the --force flag has been set
-    if dst_path.exists() and force == False:
-        sys.stderr.write(f"The path '{dst_path}' already exists\n")
-        sys.exit(1)
+@export.command("profile")
+@click.argument("name")
+@click.option("--force", "-f", is_flag=True, default=False)
+def profile(name: str, force: bool) -> None:
+    # Ensure <conan_home>/profiles exists
+    Path.mkdir(conan_home/"profiles", parents=True, exist_ok=True)
 
-    # Ensure parent directories exist
-    dst_path.parent.mkdir(parents=True, exist_ok=True)
-
-    # Copy the command to <conan_home>/extensions/commands/
-    if recursive:
-        shutil.copytree(src_path, dst_path, symlinks=True, dirs_exist_ok=True)
-        return
-    shutil.copy2(src_path, dst_path.parent)
+    _copy(conan_repo/"profiles"/name, conan_home/"profiles"/name, force)
+    return
 
 if __name__ == "__main__":
     export()
+    exit(0)
